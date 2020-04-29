bdaaf80b998177e2acd47289f5978d329113510e
Isaac Elesia <isaacelesia@gmail.com>
2020-04-29 13:17:57 -0500

    init commit

---
+++ src/server.js
@@ -0,0 +1,5 @@
+const app = require('./');
+
+app.listen(8000, () => {
+	console.log('Server started on Port 8000');
+});
---
+++ test/app.test.js
@@ -0,0 +1,5 @@
+const app = require('../scr/app');
+const { expect } = require('chai');
+const supertest = require('supertest');
+
+describe('app modules', () => {});
